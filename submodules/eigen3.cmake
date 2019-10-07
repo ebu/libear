89cfa5a7b7d0a5f836a66347d829d3393b74543c
Tom Nixon <tom@tomn.co.uk>
2019-10-07 11:25:50 +0100

    complain during configure if Eigen is missing
    Closes #12

--- submodules/eigen3.cmake
+++ submodules/eigen3.cmake
@@ -1,3 +1,10 @@
+if(NOT EXISTS "${PROJECT_SOURCE_DIR}/submodules/eigen/Eigen/Eigen")
+    message(SEND_ERROR
+        "Eigen submodule has not been cloned; please run:\n"
+        "git submodule update --init --recursive"
+    )
+endif()
+
 add_library(Eigen3::Eigen INTERFACE IMPORTED GLOBAL)
 set_target_properties(Eigen3::Eigen PROPERTIES
   INTERFACE_INCLUDE_DIRECTORIES $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/submodules/eigen>
