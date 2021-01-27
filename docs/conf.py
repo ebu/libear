b0daef21a43618f74004050a4e960ef975a63233
Tom Nixon <tom@tomn.co.uk>
2021-01-27 18:24:16 +0000

    fix bibtex extension in docs

--- docs/conf.py
+++ docs/conf.py
@@ -78,6 +78,9 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+# configure sphinxcontrib.bibtex
+bibtex_bibfiles = ['refs.bib']
+
 # make a pybtex style which uses the bibtex key for labels
 
 from pybtex.style.formatting.unsrt import Style as UnsrtStyle
